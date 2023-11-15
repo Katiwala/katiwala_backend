cd30859f6e02dc690a80cccad0c92444adfad9e0
Kamiko2021 <klopez_ccs@uspf.edu.ph>
2023-11-16 01:20:55 +0800

    fist commit

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ dotenv.config()
 const app=express()
 mongoose.connect(process.env.DATABASE_URL)
 const db=mongoose.connection
+
 db.on('error', function (error){ console.error(error)})
 db.once('open', function (open){ console.log("Connected to database")})
 
+app.use(express.json())
 app.listen(3000, function(){
     console.log("Server Connected")
 })
