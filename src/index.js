b2433fa779dbc49e174e86d01e92326fe19ebf43
marlonG <mgacrama_ccs@uspf.edu.ph>
2023-11-17 10:24:50 +0800

    Add cors and cookie-parser middleware, update .gitignore

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import * as dotenv from 'dotenv'
-dotenv.config()
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-const app=express()
-mongoose.connect(process.env.DATABASE_URL)
-const db=mongoose.connection
+const app = express();
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
 
-db.on('error', function (error){ console.error(error)})
-db.once('open', function (open){ console.log("Connected to database")})
+db.on("error", function (error) {
+  console.error(error);
+});
+db.once("open", function (open) {
+  console.log("Connected to database");
+});
 
-app.use(express.json())
-app.listen(3000, function(){
-    console.log("Server Connected")
-})
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.listen(3000, function () {
+  console.log("Server Connected");
+});
