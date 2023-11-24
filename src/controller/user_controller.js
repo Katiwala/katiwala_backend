f2c77659ab27820a799d1106888d38d26c3f7906
Kamiko2021 <klopez_ccs@uspf.edu.ph>
2023-11-25 01:15:00 +0800

    user_add in controller

--- src/controller/user_controller.js
+++ src/controller/user_controller.js
@@ -1,12 +1,31 @@
+import user_model from "../models/user_model"
+
 export const user_add = (request, response) => {
-    const user = request.body
+    const user = new user_model({
+        username:request.body.username,
+        fname:request.body.fname,
+        lname:request.body.lname,
+        address:request.body.address,
+        email:request.body.email,
+        password:request.body.password,
+        phone_number:request.body.phone_number,
+        date_of_birth:request.body.date_of_birth,
+        profile_picture:request.body.profile_picture,
+        documents:request.body.documents,
+        account_balance:request.body.account_balance,
+        account_rating:request.body.account_rating,
+        is_validated:request.body.is_validated,
+        access_level:request.body.access_level
+    })
+    const user_get=user.save()
+    response.status(201).json(user)
 }
 export const user_get = (request, response) => {
-    const user = request.body
+    
 }
 export const user_update = (request, response) => {
-    const user = request.body
+    
 }
 export const user_delete = (request, response) => {
-    const user = request.body
+    
 }
