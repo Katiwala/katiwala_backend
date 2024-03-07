0bed60963df51e8ba6d2090a384447a5f312dc77
marlonG <mgacrama_ccs@uspf.edu.ph>
2024-03-07 18:14:09 +0800

    Add user existence check before creating a new user

--- src/controller/user_controller.js
+++ src/controller/user_controller.js
@@ -8,6 +8,17 @@ export const user_add = async (request, response) => {
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
 
+  //we need to check first if the user already exists using the phoneNumber
+  const userExists = await user_model
+    .findOne({ phoneNumber: request.body.phoneNumber })
+    .exec();
+
+  if (userExists) {
+    return response.status(HttpErrorCodes.BadRequest).json({
+      message: "User already exists please use a different phone number",
+    });
+  }
+
   try {
     const user = new user_model({
       ...request.body,
@@ -17,7 +28,6 @@ export const user_add = async (request, response) => {
     //? This will store the data in the database MONGODB
     const user_get = await user.save();
     response.status(HttpSuccessCodes.Created).json(user_get);
-    response.status(HttpSuccessCodes.Created).json(user);
   } catch (err) {
     response.status(HttpErrorCodes.InternalServerError).json(err);
   }
