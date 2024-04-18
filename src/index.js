6f786360145db2ab345a7652f3f032f84bc7ff82
marlonG <mgacrama_ccs@uspf.edu.ph>
2024-04-18 18:44:07 +0800

    Refactor user_add function to include Firebase storage for image uploads and update request body limits

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as dotenv from "dotenv";
 import user_router from "./routes/user_router.js";
 import login_router from "./routes/login_router.js";
 import helmet from "helmet";
+import { limit } from "firebase/firestore";
 
 dotenv.config();
 
@@ -21,7 +22,8 @@ db.once("open", function (open) {
 });
 
 app.use(helmet());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
 app.use(cookieParser());
 
